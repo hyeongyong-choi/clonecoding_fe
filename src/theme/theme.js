23ac839c33de49f34762f2befb9305a7657c4f33
suzzeong <su981006@naver.com>
2022-08-19 16:56:59 +0900

    버튼, 인풋 수정

---
+++ src/theme/theme.js
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#0d6efd',
+  black: '#000000',
+  white: '#ffffff',
+  danger: '#ff0000',
+  dangerhover: '#cc0000',
+  disabled: '#D6D6D6',
+  disabledText: '#999999',
+  boxColor: '#e9e9e9',
+  placeholder: '#8e8e8e',
+};
